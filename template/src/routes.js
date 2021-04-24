1952a116f135b2b26736411468c7862360c24d63
Madson Chavante <ml.chavante@gmail.com>
2021-04-24 03:08:19 -0300

    init

---
+++ template/src/routes.js
@@ -0,0 +1,88 @@
+
+import * as React from 'react';
+import { View, Text, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Modal from 'react-native-modal';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+
+
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function HomeScreen({ navigation }) {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Home Screen</Text>
+            <Button
+                onPress={() => navigation.navigate('Details')}
+                title="Open Details"
+            />
+        </View>
+    );
+}
+
+function DetailsScreen() {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Details Screen</Text>
+        </View>
+    );
+}
+
+function SettingsScreen() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Settings!</Text>
+        </View>
+    );
+}
+
+const MainStackScreen = () => {
+    return (
+        <Tab.Navigator
+            screenOptions={({ route }) => ({
+                headerShown: false,
+                tabBarIcon: ({ focused, color, size }) => {
+                    let iconName;
+
+                    if (route.name === 'Home') {
+                        iconName = focused
+                            ? 'home-variant'
+                            : 'home-variant-outline';
+                    } else if (route.name === 'Details') {
+                        iconName = focused ? 'account-details' : 'account-details-outline';
+                    }else if (route.name === 'Settings') {
+                        iconName = focused ? 'cog' : 'cog-outline';
+                    }
+
+                    // You can return any component that you like here!
+                    return <Icon name={iconName} size={size} color={color} />;
+                },
+            })}
+            tabBarOptions={{
+                activeTintColor: 'tomato',
+                inactiveTintColor: 'gray',
+            }}>
+            <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="Details" component={DetailsScreen} />
+            <Tab.Screen name="Settings" component={SettingsScreen} />
+        </Tab.Navigator>
+    );
+};
+
+const Routes = () => {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator
+                screenOptions={{
+                    headerShown: false
+                }}>
+                <Stack.Screen name="MainStackScreen" component={MainStackScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+};
+
+export default Routes;
